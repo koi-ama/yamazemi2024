3feab2350aa435b7c1605cefa3f31dc953233a09
koi-ama-m3p <koiama@amakasukiichirounoMacBook-Pro.local>
2024-06-29 15:19:17 +0900

    Update configurations for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     experimental: {
-      appDir: true, // appディレクトリを使用する場合はこの設定が必要です
+      appDir: true,
     },
-  }
+  };
   
-  export default nextConfig
+  module.exports = nextConfig;
